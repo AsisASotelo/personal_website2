ab540e4eb6096e288dc35a91450cb859ebc3ddfa
AsisASotelo <asis.a.sotelo@gmail.com>
2020-08-25 20:05:53 -0700

    Add Pagination bar

--- pwmk2/src/sections/ProjSec.jsx
+++ pwmk2/src/sections/ProjSec.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
+import { useState } from 'react';
 import FooterComp from '../components/FooterComp';
 import TitleComp from '../components/TitleComp';
 import HeaderComp from '../components/HeaderComp';
 import CardComp from '../components/CardComp';
 import {PROJECTS} from '../services/projects'
+import Pagination  from '../common/pagination';
 
 function ProjSec(props) {
 
+    const [itemsCount,setItemsCount] = useState(0);
+    const [projects,setProjects] = useState(PROJECTS);
+    const [currentPage,setCurrentPage] = useState(1);
+    const [pageSize, setPageSize] = useState(3)
 
     return (
        <>
         <HeaderComp/>
         <TitleComp title="Projects" subtitle="A collection of my work..."/>
         <CardComp 
-            items={PROJECTS}/>
+            items={projects}/>
+        <Pagination
+            itemsCount ={pageSize.length} 
+            pageSize={pageSize}
+            currentPage = {currentPage} 
+            />
         <FooterComp/>
         
        </>
