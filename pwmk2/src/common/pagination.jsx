194f281fa3863a2a01db28265392dcbadd8bd9f5
AsisASotelo <asis.a.sotelo@gmail.com>
2020-08-26 20:33:07 -0700

    Edit Pagination

--- pwmk2/src/common/pagination.jsx
+++ pwmk2/src/common/pagination.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
+import { Container } from 'react-bootstrap';
 
 
 const Pagination = props => {
@@ -15,15 +16,19 @@ const Pagination = props => {
 
 
     return(
-        <nav>
-            <ul className="pagination">
-                {pages.map(page => (
-                    <li key ={page} className={page === currentPage ? 'page-item active':'page-item'}>
-                        <a className="page-link" onClick = {()=> onPageChange(page)} >{page}</a>
-                    </li>
-                ))}
-            </ul>
-        </nav>
+        <Container >
+
+            <nav className="proj-page">
+                <ul className="pagination">
+                    {pages.map(page => (
+                        <li key ={page} className={page === currentPage ? 'page-item active':'page-item'}>
+                            <a className="page-link" onClick = {()=> onPageChange(page)} >{page}</a>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+
+        </Container>
 
 
     )
