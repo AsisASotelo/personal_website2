33b467601a4e3dc24c2bdb2a0abe50ca9eda22c7
AsisASotelo <asis.a.sotelo@gmail.com>
2020-09-26 18:13:36 -0700

    Edit Project Page

--- pwmk2/src/services/projects.js
+++ pwmk2/src/services/projects.js
@@ -14,7 +14,7 @@ export const PROJECTS = [{
         _id: 1,
         title: "NY Gyms Clustering",
         card_image: "/assets/images/cluster_gyms.png",
-        body: "Final project of the IBM Data Science Specialization. The entire data science methodology, which begins with identifying the problem. In this case can we determine which borough of New York is best for gym accesibility and quality. Data from gym was scraped utilizing the Foursquare API."
+        body: "The entire data science methodology, which begins with identifying the problem. In this case can we determine which borough of New York is best for gym accesibility and quality.Rating algorithm enabled a comparison across different price points."
 
 
     },
@@ -23,7 +23,7 @@ export const PROJECTS = [{
         _id: 2,
         title: "Mandelbulb Creator",
         card_image: "/assets/images/mandelbulb.png",
-        body: "Creates an image of a Mandelbulb which is a 3d extension of the 2-d Mandelbrot set. The image is 500 by 500 pixels and implements distance estimation for the ray tracing allowing some shadowing to appear in the image. Computational complexity causes a loading time of 3 minutes on average. All of the work was done on a Raspberry PI 3B+. "
+        body: "Creates an image of a Mandelbulb which is a 3d extension of the 2-d Mandelbrot set. The image is 500 by 500 pixels and implements distance estimation with ray tracing allowing some shadowing to appear in the image.  All of the work was done on a Raspberry PI 3B+. "
 
 
     },
@@ -32,7 +32,7 @@ export const PROJECTS = [{
         _id: 3,
         title: "HDFS Inverter",
         card_image: "/assets/images/inverter.png",
-        body: "Analyzed the Hadoop Distributed file system I tokenized the file and directory names of a Hadoop cluster Name Node deployed on Amazon Linux EC2 instances. I then wrote a python script to take an xml file and then produce another xml file known as an inverted index. Listing the file/directory names inode numbers."
+        body: "Tokenization of the file and directory names of a Hadoop cluster Name Node deployed on Amazon Linux EC2 instances. I then wrote a python script to take an xml file and then produce another xml file known as an inverted index. Listing the file/directory names inode numbers."
 
 
     }
